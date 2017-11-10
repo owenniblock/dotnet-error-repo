687ef496beaa2fa7c8f3c95bac9923a70be0ae36
Owen Niblock <me@owenniblock.com>
2017-11-10 11:38:39 +0000

    Repo code for dotnet test -f net461 error

---
+++ ExampleTest/MyTestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExampleTest
+{
+    [TestClass]
+    public class MyTestClass
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+        }
+    }
+}
